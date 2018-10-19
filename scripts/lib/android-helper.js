f5490f9cf03e1f58d5fa7c8e6b001ce597efe7c1
Igor Martsekha <igor.martsekha@gmail.com>
2018-10-19 17:03:29 +0300

    Remove "googel()" repository

--- scripts/lib/android-helper.js
+++ scripts/lib/android-helper.js
@@ -40,9 +40,9 @@ function addRepos(buildGradle) {
   var whitespace = match[1];
 
   // modify the line to add the necessary repo
-  var googlesMavenRepo = whitespace + 'google() // Google\'s Maven repository from cordova-plugin-firebase';
+  // var googlesMavenRepo = "";//whitespace + 'google() // Google\'s Maven repository from cordova-plugin-firebase';
   var fabricMavenRepo = whitespace + 'maven { url \'https://maven.fabric.io/public\' } // Fabrics Maven repository from cordova-plugin-firebase'
-  var modifiedLine = match[0] + '\n' + googlesMavenRepo + '\n' + fabricMavenRepo;
+  var modifiedLine = match[0] + /*'\n' + googlesMavenRepo +*/ '\n' + fabricMavenRepo;
 
   // modify the actual line
   return buildGradle.replace(/^(\s*)jcenter\(\)/m, modifiedLine);
